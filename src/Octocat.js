e816975c11fe3d9e6f7b05fe95c33a94579018a7
Allan <allan@Allans-MBP.localdomain>
2018-09-24 15:53:47 -0400

    reactocat

---
+++ src/Octocat.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+class Octocat extends Component {
+    state = {}
+    render() {
+        return (
+            <article className="cat-element">
+                <div className="top-image">
+                    <img className="main-image" src={this.props.image} />
+                </div>
+                <div className="image-footer">
+                    <p className="number">#{this.props.number}</p>
+                    <p>the
+                            <a href={this.props.link} >{this.props.name} </a>
+                        by
+                        <img className="author-icon" src={this.props.avatar} />
+                    </p>
+                </div>
+            </article>
+
+        )
+    }
+}
+
+export default Octocat;
